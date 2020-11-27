d9f842dc8b40925afd1613204be963778fa11a76
Antor Mahmud <ontormahmud7877@gmail.com>
2020-11-27 20:35:27 +0600

    for birthday card

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Md.Sakib hasan',
+      age: 22,
+      image:
+        'https://scontent.fcgp10-1.fna.fbcdn.net/v/t1.0-9/125262126_2996481770580987_735689546370522490_o.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=hTwXzj4LMT8AX9xJura&_nc_ht=scontent.fcgp10-1.fna&oh=d8085d50ad618080d0a60631fc0136dd&oe=5FE5E4F1',
+    },
+    {
+      id: 2,
+      name: 'Ahtesham Bhuiyan',
+      age: 23,
+      image:
+        'https://scontent.fcgp10-1.fna.fbcdn.net/v/t1.0-9/83437930_638897556919672_8390897578346020864_n.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=ATkDttIPQi0AX_BmlCm&_nc_ht=scontent.fcgp10-1.fna&oh=393ffc82bd6b79a22b697f10768747fd&oe=5FE69F8A',
+    },
+    {
+      id: 3,
+      name: 'Hasan Rahman',
+      age: 23,
+      image:
+        'https://scontent.fcgp10-1.fna.fbcdn.net/v/t1.0-9/54516852_2287336434921235_1577378390732701696_n.jpg?_nc_cat=107&ccb=2&_nc_sid=09cbfe&_nc_ohc=gG_INCk1J8sAX9n0-eU&_nc_ht=scontent.fcgp10-1.fna&oh=0361afde91e9949d7dc684e19c0ba5e2&oe=5FE54AC7',
+    },
+    {
+      id: 4,
+      name: 'prashenjit',
+      age: 23,
+      image:
+        'https://scontent.fcgp10-1.fna.fbcdn.net/v/t1.0-9/47238362_1186668311486062_7317760202273456128_o.jpg?_nc_cat=107&cb=846ca55b-311e05c7&ccb=2&_nc_sid=09cbfe&_nc_ohc=NQ9ouNEFuZkAX-OINvU&_nc_ht=scontent.fcgp10-1.fna&oh=88c6cdf08b21ba703bed14fe290eebd7&oe=5FE488E7',
+    },
+    {
+      id: 5,
+      name: 'Antor Mahmud',
+      age: 29,
+      image:
+        'https://scontent.fcgp10-1.fna.fbcdn.net/v/t1.0-9/89356860_1104995803182495_7350392912816898048_o.jpg?_nc_cat=107&ccb=2&_nc_sid=09cbfe&_nc_ohc=El4rApBYHW4AX_BhA1W&_nc_ht=scontent.fcgp10-1.fna&oh=f549af22c00525133ba4e2f2c038c64d&oe=5FE4C9D8',
+    },
+  ];
